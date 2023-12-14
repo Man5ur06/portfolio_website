5c0f1a67f3b79bdd2b6745aca87640c85e620943
Mansur Bizhanov <mansur.bizhanov@gmail.com>
2023-12-14 19:17:35 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./app.scss"
 import Navbar from "./components/Navbar/Navbar";
-import { About } from "./components/about_me/About";
+import {About} from "./components/about_me/About";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
@@ -14,10 +14,10 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      {/* <section id="About"><Parallax type="about"/></section> */}
-      {/* <section><About /></section> */}
-      {/* <section id="Portfolio"><Parallax type="portfolio"/></section> */}
-      {/* <Portfolio /> */}
+      <section id="About"><Parallax type="about"/></section> 
+      <section><About /></section> 
+      <section id="Portfolio"><Parallax type="portfolio"/></section> 
+      <Portfolio /> 
       {/* <section id="Contact"><Contact/></section> */}
     </div>
   )
